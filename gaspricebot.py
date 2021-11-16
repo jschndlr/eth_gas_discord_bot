c33725af411e019d2d7364dda434e7dbd91522c3
antij0sh <59179111+antij0sh@users.noreply.github.com>
2021-11-16 09:18:42 -0500

    Initial

---
+++ gaspricebot.py
@@ -0,0 +1,45 @@
+#! python3
+# gaspricebot.py
+import os
+import discord
+from PIL import Image
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+#Load environment variables
+load_dotenv()
+url = os.getenv('TARGET_URL')
+path = os.getenv('TARGET_FILE')
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+#Grab image through chromedriver using selenium
+async def getgas():
+    driver = webdriver.Chrome()
+    driver.get(url)
+    el = driver.find_element(By.ID, "body")
+    el.screenshot(path)
+    crop_image()
+
+#Crop image and re-save
+def crop_image():
+    crop = Image.open(path).crop((0,168,919,709))
+    crop.save('scrape.png')
+
+#Discord bot and events
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!gas'):
+        await getgas()
+        await message.channel.send(file=discord.File(path))
+
+try:
+    client.run(TOKEN)
+except:
+    print('client connect exception, check internet connection')
